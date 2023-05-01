08fd65302336f078d2d6b7427545202a0443fff9
Joel <moipanpan@hotmail.fr>
2023-05-01 17:50:06 +0200

    deuxieme commit

--- clients/enregistrementClient.php
+++ clients/enregistrementClient.php
@@ -1,25 +1,34 @@
 <?php
 session_start();
-@$email = $_POST["email"];
-@$mot_de_passe =  $_POST["mot_de_passe"];
-@$valider = $_POST["valider"];
-$erreur = "";
 
-if (isset($valider)) {
-   include("connexion.php");
-   $sel = $pdo->prepare("select * from clients where email=? and mot_de_passe=? limit 1"); // 
-   $sel->execute(array($email, $mot_de_passe));
-   $tab = $sel->fetchAll();
-
-   if (count($tab) > 0) {
-      $_SESSION["prenomNom"] = ucfirst(strtolower($tab[0]["prenom"])) .
-         " " . strtoupper($tab[0]["nom"]);
-      $_SESSION["autoriser"] = "oui";
-      header("location:reservation.php");
-   } else {
-      $erreur = "Mauvais login ou mot de passe!";
-   }
+if (isset($_POST['valider'])) {
+   $erreur = login($_POST['email'], $_POST['mot_de_passe']);
+   echo $erreur;
 }
+
+function login($login, $password)
+{
+   require 'connexion.php';
+
+   $statement = $pdo->prepare('select * from clients where email=? limit 1');
+   $statement->execute([$login]);
+   $account = $statement->fetch();
+
+   if (false === $account) {
+      return 'Compte inconnu.';
+   }
+
+   if (false === password_verify($password, $account['mot_de_passe'])) {
+      return 'Mot de passe  Email ou non valide.';
+   }
+
+   $_SESSION['prenomNom'] = ucfirst(strtolower($account['prenom'])) . ' ' . strtoupper($account['nom']);
+   $_SESSION['autoriser'] = "oui";
+
+   header('location: reservation.php');
+   exit;
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -30,89 +39,10 @@ if (isset($valider)) {
 
    <meta charset="utf-8" />
 
-   <style>
-      .breadcrumb {
+   <title>Login</title>
 
-         padding: 0 .5rem;
+   <link rel="stylesheet" href="ClientCss.css">
 
-      }
-
-      .breadcrumb ul {
-
-         display: flex;
-
-         flex-wrap: wrap;
-
-         list-style: none;
-
-         margin: 0;
-
-         padding: 30px;
-
-      }
-
-      .breadcrumb li:not(:last-child)::after {
-
-         display: inline-block;
-
-         margin: 0 .25rem;
-
-         content: ">>";
-
-      }
-
-      * {
-
-         font-family: arial;
-
-      }
-
-      body {
-
-         margin: 20px;
-
-      }
-
-      input {
-
-         border: solid 1px #2222AA;
-
-         margin-bottom: 10px;
-
-         padding: 16px;
-
-         outline: none;
-
-         border-radius: 6px;
-
-      }
-
-      .erreur {
-
-         color: #CC0000;
-
-         margin-bottom: 10px;
-
-      }
-
-      a {
-
-         font-size: 12pt;
-
-         color: #EE6600;
-
-         text-decoration: none;
-
-         font-weight: normal;
-
-      }
-
-      a:hover {
-
-         text-decoration: underline;
-
-      }
-   </style>
 
 </head>
 
@@ -134,17 +64,65 @@ if (isset($valider)) {
 
    </nav>
 
-   <h1>Authentification [ <a href="inscriptionClient.php">Créer un compte</a> ]</h1>
+   <h1>Authentification [ <a href="formulaireClient.php">Créer un compte</a> ]</h1>
 
-   <div class="erreur"><?php echo $erreur ?></div>
 
-   <form name="fo" method="post" action="">
 
-      <input type="text" name="email" placeholder="email"  /><br />
 
-      <input type="password" name="mot_de_passe" placeholder="Mot de passe" /><br />
+   <form id="email-form" name="email-form" method="post" action=""><!-- name="fo" method="post" -->
 
-      <input type="submit" name="valider" value="S'authentifier" />
+      <table width="100%" border="0" align="center" cellpadding="4" cellspacing="1">
+
+         <tr>
+
+            <td>
+
+               <div class="label">Email:</div>
+
+               <div class="field">
+
+                  <input type="text" name="email" placeholder="email" /><br /><br><br>
+
+
+               </div>
+
+            </td>
+
+         </tr>
+
+         <tr>
+
+            <td>
+
+               <div class="label">Mot de passe:</div>
+
+               <div class="field">
+
+                  <input type="password" name="mot_de_passe" placeholder="Mot de passe" /><br />
+                  <br /><br>
+
+
+               </div>
+
+            </td>
+
+         </tr>
+
+         <tr>
+
+            <td>
+
+               <div class="field">
+
+                  <input type="submit" name="valider" value="S'authentifier" /><br>
+
+               </div>
+
+            </td>
+
+         </tr>
+
+      </table>
 
    </form>
 
